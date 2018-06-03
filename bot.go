f0f384486179c0635a35c99f2f73b834825e0222
Ron Barabash <rbarabash@yotpo.com>
2018-06-03 19:23:01 +0300

    initial bot

---
+++ bot.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/maciekmm/messenger-platform-go-sdk"
+)
+
+func main() {
+	messenger := &messenger.Messenger{
+		VerifyToken: os.Getenv("VERIFY_TOKEN"),
+		AppSecret:   os.Getenv("APP_SECRET"),
+		AccessToken: os.Getenv("PAGE_ACCESS_KEY"),
+	}
+
+
+	//dbConnectionParams := providers.DBConnectionParams{
+	//	User:     os.Getenv("DB_USER"),
+	//	Password: os.Getenv("DB_PASS"),
+	//	Address:  os.Getenv("DB_ADDRESS"),
+	//	DBName:   os.Getenv("DB_NAME"),
+	//}
+
+	//dataProvider, err := providers.NewBotDataProvider(dbConnectionParams)
+	//if err != nil {
+	//	fmt.Println("could not create suchef server. error: " + err.Error())
+	//	return
+	//}
+
+	//accountID := int64(1)
+
+	//suchefServer := server.NewSuchefServer(accountID, messenger, dataProvider, dataProvider, dataProvider)
+	//fmt.Println("server started successfully")
+
+	//messenger.MessageReceived = suchefServer.BindMessageReceived()
+	//messenger.Postback = suchefServer.BindPostbackReceived()
+
+	r := mux.NewRouter()
+	r.HandleFunc("/webhook", messenger.Handler)
+	http.Handle("/", r)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+}
